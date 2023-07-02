c4212eb15b58e0a7bbfacd861daa2d5745a05c4f
Chethan hn <chethan9@hotmail.com>
2023-07-02 13:37:55 +0300

    Update player_widget.dart

--- lib/pages/player/player_widget.dart
+++ lib/pages/player/player_widget.dart
@@ -1,23 +1,14 @@
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_video_player.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:meedu_player/meedu_player.dart';
 import 'player_model.dart';
-export 'player_model.dart';
 
 class PlayerWidget extends StatefulWidget {
+  final String? videoUrl;
+
   const PlayerWidget({
     Key? key,
-    this.videolinkstring,
-    required this.videopath,
+    this.videoUrl,
   }) : super(key: key);
-
-  final String? videolinkstring;
-  final String? videopath;
 
   @override
   _PlayerWidgetState createState() => _PlayerWidgetState();
@@ -26,137 +17,25 @@ class PlayerWidget extends StatefulWidget {
 class _PlayerWidgetState extends State<PlayerWidget> {
   late PlayerModel _model;
 
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
   @override
   void initState() {
     super.initState();
-    _model = createModel(context, () => PlayerModel());
-
-    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+    _model = PlayerModel();
+    _model.setVideoSource(widget.videoUrl!);
   }
 
   @override
   void dispose() {
     _model.dispose();
-
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    context.watch<FFAppState>();
-
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: SafeArea(
-          top: true,
-          child: Align(
-            alignment: AlignmentDirectional(0.0, 0.0),
-            child: Container(
-              width: MediaQuery.sizeOf(context).width * 0.9,
-              height: MediaQuery.sizeOf(context).height * 0.9,
-              decoration: BoxDecoration(
-                color: FlutterFlowTheme.of(context).secondaryBackground,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FFButtonWidget(
-                        onPressed: () async {
-                          await launchURL(widget.videolinkstring!);
-                        },
-                        text: 'Download',
-                        icon: Icon(
-                          Icons.downloading_rounded,
-                          size: 35.0,
-                        ),
-                        options: FFButtonOptions(
-                          height: 60.0,
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              24.0, 0.0, 24.0, 0.0),
-                          iconPadding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: Color(0xFF4CA76E),
-                          textStyle:
-                              FlutterFlowTheme.of(context).titleSmall.override(
-                                    fontFamily: 'Readex Pro',
-                                    color: Colors.white,
-                                    fontSize: 20.0,
-                                  ),
-                          borderSide: BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                      FFButtonWidget(
-                        onPressed: () async {
-                          context.goNamed(
-                            'Home',
-                            extra: <String, dynamic>{
-                              kTransitionInfoKey: TransitionInfo(
-                                hasTransition: true,
-                                transitionType: PageTransitionType.fade,
-                                duration: Duration(milliseconds: 0),
-                              ),
-                            },
-                          );
-                        },
-                        text: 'home',
-                        icon: Icon(
-                          Icons.home_outlined,
-                          size: 35.0,
-                        ),
-                        options: FFButtonOptions(
-                          height: 60.0,
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              24.0, 0.0, 24.0, 0.0),
-                          iconPadding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: Color(0xFF4CA76E),
-                          textStyle:
-                              FlutterFlowTheme.of(context).titleSmall.override(
-                                    fontFamily: 'Readex Pro',
-                                    color: Colors.white,
-                                    fontSize: 20.0,
-                                  ),
-                          borderSide: BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ].divide(SizedBox(width: 15.0)),
-                  ),
-                  FlutterFlowVideoPlayer(
-                    path: widget.videopath!,
-                    videoType: VideoType.network,
-                    width: 640.0,
-                    height: 480.0,
-                    aspectRatio: 1.70,
-                    autoPlay: false,
-                    looping: true,
-                    showControls: true,
-                    allowFullScreen: true,
-                    allowPlaybackSpeedMenu: true,
-                    lazyLoad: false,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: MeeduVideoPlayer(
+        controller: _model.playerController,
       ),
     );
   }
